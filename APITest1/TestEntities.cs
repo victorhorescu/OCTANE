ac45d6ed35742927f7a129e681895757590b8ba1
Victor <victor.horescu@iqst.ro>
2019-11-06 22:54:16 +0200

    commit UFT tests

---
+++ APITest1/TestEntities.cs
@@ -0,0 +1,40 @@
+using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using HP.ST.Fwk.RunTimeFWK.Utilities;
+    using HP.ST.Fwk.RunTimeFWK.BindingFWK;
+    
+    namespace Script
+    {
+    
+    public class TestEntities
+    {
+    public ISTRunTimeContext Context = null;
+    public Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource> dataSourceNameToDataSource = new Dictionary<string, HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource>();
+    
+    protected HP.ST.Fwk.RunTimeFWK.DataHandling.IDataSource GetDataSource(string dataSourceName)
+    {
+    if(!dataSourceNameToDataSource.ContainsKey(dataSourceName))
+    	throw new Exception(("A data source with the specified name does not exist."));
+    return dataSourceNameToDataSource[dataSourceName];
+    }
+    public HP.ST.Ext.BasicActivities.DataFetchActivity DataFetchActivity13 = null;
+    public HP.ST.Ext.BasicActivities.DataFetchActivity DataFetchActivity10 = null;
+    public HP.ST.Ext.BasicActivities.StartActivity StartActivity1 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Loop<Loop2Input> Loop2 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterActivity DataExporterActivity12 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterActivity DataExporterActivity15 = null;
+    public HP.ST.Ext.BasicActivities.DataExporterCloseActivity DataExporterCloseActivity16 = null;
+    public HP.ST.Ext.BasicActivities.EndActivity EndActivity3 = null;
+    public HP.ST.Ext.BasicActivities.DataDisconnectActivity DataDisconnectActivity11 = null;
+    public HP.ST.Ext.BasicActivities.DataDisconnectActivity DataDisconnectActivity14 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Sequence Sequence8 = null;
+    public HP.ST.Ext.WebServicesActivities.StServiceCallActivity StServiceCallActivity4 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Loop<Loop7Input> Loop7 = null;
+    public HP.ST.Fwk.RunTimeFWK.CompositeActivities.Sequence Sequence9 = null;
+    public HP.ST.Ext.WebServicesActivities.StServiceCallActivity StServiceCallActivity6 = null;
+    
+    }
+    
+    }
+    
--- GUITest2/Action1/Script.mts
+++ GUITest2/Action1/Script.mts
@@ -1,1 +1,11 @@
-﻿
+﻿WpfWindow("Micro Focus MyFlight Sample").WpfEdit("agentName").Set "John" @@ hightlight id_;_2023384632_;_script infofile_;_ZIP::ssf2.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfEdit("password").SetSecure "5dc163dc62e326bae9d4" @@ hightlight id_;_1907637600_;_script infofile_;_ZIP::ssf3.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfButton("OK").Click @@ hightlight id_;_2023386984_;_script infofile_;_ZIP::ssf4.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfComboBox("fromCity").Select "Paris" @@ hightlight id_;_1880484704_;_script infofile_;_ZIP::ssf8.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfComboBox("toCity").Select "San Francisco" @@ hightlight id_;_2023389912_;_script infofile_;_ZIP::ssf10.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfButton("FIND FLIGHTS").Click @@ hightlight id_;_2023390200_;_script infofile_;_ZIP::ssf11.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfTable("flightsDataGrid").SelectCell 0,2 @@ hightlight id_;_2023391304_;_script infofile_;_ZIP::ssf12.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfButton("SELECT FLIGHT").Click @@ hightlight id_;_2023391544_;_script infofile_;_ZIP::ssf13.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfEdit("passengerName").Set "aaa" @@ hightlight id_;_2023390632_;_script infofile_;_ZIP::ssf14.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").WpfButton("ORDER").Click @@ hightlight id_;_1907618688_;_script infofile_;_ZIP::ssf15.xml_;_
+WpfWindow("Micro Focus MyFlight Sample").Close @@ hightlight id_;_199432_;_script infofile_;_ZIP::ssf16.xml_;_
